b49274bd339bc236a1a2f6147f74ffe3db2792c9
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-07-10 21:34:24 +0300

    first commit

---
+++ app/src/main/java/com/kirigo/twitterapplication/Tweet.kt
@@ -0,0 +1,14 @@
+package com.kirigo.twitterapplication
+
+data class Tweet(
+    var name:String,
+    var word:String,
+    var reply:String,
+    var retweet:String,
+    var react:String,
+    var share:String,
+    var image:String,
+    var num:String,
+    var num1:String,
+    var num2:String,
+    var num3:String,)
---
+++ app/src/main/java/com/kirigo/twitterapplication/TweetRVAdapter.kt
@@ -0,0 +1,43 @@
+package com.kirigo.twitterapplication
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TweetRVAdapter(var tweetList:List<Tweet>):
+    RecyclerView.Adapter<TweetViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):TweetViewHolder {
+        var  itemView=LayoutInflater.from(parent.context)
+            .inflate(R.layout.tweet_list_items, parent, false)
+        return TweetViewHolder(itemView)
+
+    }
+
+    override fun onBindViewHolder(holder: TweetViewHolder, position: Int) {
+        var currentTweet = tweetList.get(position)
+//        holder.tvName.text = currentTweet.name
+//        holder.tvWords.text = currentTweet.word
+//        holder.tvNum.text = currentTweet.num
+//        holder.tvNum1.text = currentTweet.num1
+//        holder.tvNum2.text = currentTweet.num2
+//        holder.tvNum3.text = currentTweet.num3
+    }
+
+
+    override fun getItemCount(): Int {
+        return tweetList.size
+    }
+}
+
+class TweetViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+//    var  tvName=itemView.findViewById<TextView>(R.id.tvName)
+//    var  tvWords=itemView.findViewById<TextView>(R.id.tvWords)
+//    var  tvNum=itemView.findViewById<TextView>(R.id.tvNum)
+//    var  tvNum1=itemView.findViewById<TextView>(R.id.tvNum1)
+//    var  tvNum2=itemView.findViewById<TextView>(R.id.tvNum2)
+//    var  tvNum3=itemView.findViewById<TextView>(R.id.tvNum3)
+//    var  imgReply=itemView.findViewById<ImageView>(R.id.imgReply)
+//    var  imgRetweet=itemView.findViewById<ImageView>(R.id.imgRetweet)
+//    var  imgReact=itemView.findViewById<ImageView>(R.id.imgReact)
+//    var  imgShare=itemView.findViewById<ImageView>(R.id.imgShare)
+}
